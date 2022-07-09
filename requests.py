fc6dd0b082b808b7a29fc9ce2e946564378ba6ce
Arnab Dhar <arnabdhar32@gmail.com>
2022-07-09 13:17:45 +0530

    Template for DB Requests

---
+++ requests.py
@@ -0,0 +1,26 @@
+from connection import Connection
+
+
+
+# Setup connection
+cnx = Connection()
+
+
+class Ledger:
+    
+    def __init__(self):
+        pass
+    
+    
+    
+class MasterChart:
+    
+    def __init__(self):
+        pass
+    
+    
+    
+class CustomChart:
+    
+    def __init__(self):
+        pass
